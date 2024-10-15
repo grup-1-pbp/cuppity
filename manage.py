bfada24001b42a9a5aab31ce32c2131ec92c30a6
Athallah210505 <n4dhif@gmail.com>
2024-10-15 14:36:51 +0700

    setup correction

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cuppity.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mangan_yuk.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- mangan_yuk/wsgi.py
+++ mangan_yuk/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cuppity.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mangan_yuk.settings')
 
 application = get_wsgi_application()
