215d4979f73b40c2cc82425dbe2ab3677bc2f0c5
Athallah210505 <n4dhif@gmail.com>
2024-10-20 19:15:12 +0700

    Commit message

--- mangan_yuk/settings.py
+++ mangan_yuk/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
-from django.contrib import admin
+
 
 
 
@@ -23,13 +20,14 @@ from django.contrib import admin
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
-
+MEDIA_URL = '/media_food_images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_food_images') 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # URL Anda lainnya
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)# Django akan membuat folder ini jika belum ada
+    # other URL patterns
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -74,7 +72,7 @@ ROOT_URLCONF = 'mangan_yuk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates', 'main')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,6 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # This is optional and useful for production
+
+# Media files (User-uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
