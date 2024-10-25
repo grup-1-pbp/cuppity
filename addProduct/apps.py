7d718d6c722c5d631a686c06275b6d4f846191cb
nayarachmi <nayaakusuma@gmail.com>
2024-10-25 16:02:49 +0700

    merapikan

--- addProduct/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class addProductConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'addProduct'
--- detailProduct/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class DetailProductConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'detailProduct'  # Ensure this matches your app folder name
